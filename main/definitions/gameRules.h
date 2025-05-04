a7e1ce4db80366fcc29dbedca4f21cb364c8b63d
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-05-04 00:31:09 -0700

    Created game rule for max player deck size, use later

--- main/definitions/gameRules.h
+++ main/definitions/gameRules.h
@@ -3,3 +3,6 @@
 
 //Set the minimum number of shuffles
 #define MINIMUM_SHUFFLE 10000
+
+//Maximum number of cards a player can have. Other player automatically wins if player tries to draw over this ammount 
+#define PLAYER_MAX_CARDS 100
